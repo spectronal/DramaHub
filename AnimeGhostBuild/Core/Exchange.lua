d07e1655dfcb30ef12b3eff7a2d18bc524a3dd1b
spectronal <guilhermekelvi@gmail.com>
2026-03-22 17:04:05 -0300

    Fixed Exchange

--- AnimeGhostBuild/Core/Exchange.lua
+++ AnimeGhostBuild/Core/Exchange.lua
@@ -7,32 +7,41 @@ local State = getgenv().DH.State
 local Framework = State.Framework
 
 function Exchange.UsePotions(mode: "1" | "2")
-	print("Fora")
 	if mode == "1" then
-		print("1")
+		local hasPotion = false
 		for potion, value in Framework.PlayerData.Inventory do
-			if potion:match("1") then
-				if value == 0 then
-					return
-				end
+			if potion:match("1") and value >= 5 then
+				hasPotion = true
+				break
+			end
+		end
 
+		if not hasPotion then
+			return
+		end
+
+		for potion, value in Framework.PlayerData.Inventory do
+			if potion:match("1") and value >= 5 then
 				local amount = value / 5
 				Framework.Remote:Fire("Exchange", "Make", "Potion", "Tier 1", potion, amount)
 			end
 		end
 	elseif mode == "2" then
-		print("2")
+		local hasPotion = false
 		for potion, value in Framework.PlayerData.Inventory do
-			print(typeof(potion))
-			if potion:match("2") then
-				print(potion, value)
-				if value == 0 then
-					print("Value 0")
-					return
-				end
+			if potion:match("2") and value >= 5 then
+				hasPotion = true
+				break
+			end
+		end
 
+		if not hasPotion then
+			return
+		end
+
+		for potion, value in Framework.PlayerData.Inventory do
+			if potion:match("2") and value >= 5 then
 				local amount = value / 5
-				print(typeof(value))
 				Framework.Remote:Fire("Exchange", "Make", "Potion", "Tier 2", potion, amount)
 			end
 		end
