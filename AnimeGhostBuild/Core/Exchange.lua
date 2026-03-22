1b102e353108ef69b7faa673050c224e4ef6e980
spectronal <guilhermekelvi@gmail.com>
2026-03-22 17:14:13 -0300

    42

--- AnimeGhostBuild/Core/Exchange.lua
+++ AnimeGhostBuild/Core/Exchange.lua
@@ -13,7 +13,7 @@ function Exchange.UsePotions(mode: "1" | "2")
 		local hasPotion = false
 		for potion, value in Framework.PlayerData.Inventory do
 			print("[Check Tier 1] Potion:", potion, "| Value:", value)
-			if potion:match("1") and value >= 5 then
+			if potion:match("1") and math.floor(value / 5) >= 1 then
 				print("[Check Tier 1] Encontrou poção válida:", potion, value)
 				hasPotion = true
 				break
@@ -51,7 +51,7 @@ function Exchange.UsePotions(mode: "1" | "2")
 		end
 
 		for potion, value in Framework.PlayerData.Inventory do
-			if potion:match("2") and value >= 5 then
+			if potion:match("2") and math.floor(value / 5) >= 1 then
 				local amount = math.floor(value / 5)
 				print("[Tier 2] Firing:", potion, "| Value:", value, "| Amount:", amount)
 				Framework.Remote:Fire("Exchange", "Make", "Potion", "Tier 2", potion, amount)
