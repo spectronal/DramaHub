5b77cffd7e2cf310dff27505dafc8b4de1adea35
spectronal <guilhermekelvi@gmail.com>
2026-03-30 22:26:59 -0300

    ,...

--- AnimeGhostBuild/UI/About.lua
+++ AnimeGhostBuild/UI/About.lua
@@ -6,8 +6,10 @@ getgenv().DH.UI = getgenv().DH.UI or {}
 getgenv().DH.UI.About = {}
 
 local About = getgenv().DH.UI.About
+local State = getgenv().DH.State
+local Framework = State.Framework
 
-function About.build(Tabs)
+function About.build(Tabs, Fluent)
 	local mainAbout = Tabs.About:AddSection("Drama Hub")
 
 	mainAbout:AddParagraph({
@@ -18,10 +20,10 @@ function About.build(Tabs)
 	mainAbout:AddButton({
 		Title = "Refresh Script",
 		Callback = function()
-			game.Players.LocalPlayer.PlayerGui:FindFirstChild("DramaHub"):Destroy()
+			Framework.Remote:Fire("AFKSystem", "Rejoin")
 			task.wait(0.5)
 			Fluent:Notify({ Title = "Drama Hub | Developer Version", Content = "Reloading...", Duration = 2 })
-			task.wait(2.2)
+			task.wait(5)
 			loadstring(game:HttpGet("https://dramahub.up.railway.app/init"))()
 		end,
 	})
