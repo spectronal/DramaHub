5f3ea2be735605364094d14a6736ee06007e63a5
spectronal <guilhermekelvi@gmail.com>
2026-03-30 21:43:36 -0300

    ...

--- AnimeGhostBuild/Core/Gamemode.lua
+++ AnimeGhostBuild/Core/Gamemode.lua
@@ -106,7 +106,7 @@ end
 -- Cooldown Helpers
 
 local function isOnCooldown(mode)
-	local remaining = State.cooldowns[mode] - tick()
+	local remaining = State.cooldowns[mode]
 	if remaining > 0 then
 	end
 	return remaining > 0.5
