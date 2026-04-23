dddde06a75d1362da2dfc908c8d6467381bd25ae
spectronal <guilhermekelvi@gmail.com>
2026-04-23 01:40:39 -0300

    a

--- Games/World Fighters/Systems/Click.lua
+++ Games/World Fighters/Systems/Click.lua
@@ -1,15 +1,13 @@
+getgenv().System = getgenv().System or {}
+getgenv().System.Click = {}
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
 local Omni = require(ReplicatedStorage:WaitForChild("Omni"))
-
-getgenv().System = getgenv().System or {}
-getgenv().System.Click = {}
 
 local Click = getgenv().System.Click
 
 function Click.Attack()
     Omni.Signal:Fire("General", "Attack", "Click")
 end
-
-return Click
